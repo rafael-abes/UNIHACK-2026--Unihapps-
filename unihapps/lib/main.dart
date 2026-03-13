f4b7d110559c3fae11dfe8314a31eb36f9f50b3a
suannlam <suannlams@gmail.com>
2026-03-14 02:33:18 +1100

    refactor: adding the import

--- unihapps/lib/main.dart
+++ unihapps/lib/main.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'pages/welcome.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'authentication-wrapper.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MyApp());
 }
