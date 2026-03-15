a72a2d1bc965e082b55bc384aef86ef6a26f7af8
rsabes <rafaelabes.1201@gmail.com>
2026-03-15 13:33:20 +1100

    fixed location tracking

--- unihapps/lib/pages/home_page.dart
+++ unihapps/lib/pages/home_page.dart
@@ -191,6 +191,8 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     initializeUser();
     getCurrentLocation();
+    listenToFriends();
+    startLocationUpdates();
   }
 
   Future<void> _loadCurrentStatus() async {
@@ -224,18 +226,6 @@ class _HomePageState extends State<HomePage> {
         children: [
           Padding(
             padding: const EdgeInsets.all(10),
-            // child: SegmentedButton<String>(
-            //   segments: const [
-            //     ButtonSegment(value: 'offline', label: Text('Offline')),
-            //     ButtonSegment(value: 'free', label: Text('Free')),
-            //     ButtonSegment(value: 'busy', label: Text('Busy')),
-            //     ButtonSegment(value: 'in-class', label: Text('In-class')),
-            //   ],
-            //   selected: {currentStatus},
-            //   onSelectionChanged: (newSelection) {
-            //     _setStatus(newSelection.first);
-            //   },
-            // ),
           ),
           Expanded(
             child: locationData == null
